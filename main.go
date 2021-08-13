6beecd263386cc515cc5bee7ed01c8f3ee40af7b
Gusyatnikova <crysnat08@gmail.com>
2021-08-13 10:55:50 +0500

    Dynamic reloading part1

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "<h1> Welcome to my awesome site!</h1>")
-
+//handlerFunc process incoming web-requests
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Welcome to Natasha's awesome site!</h1>" +
+		"<h2> Fresh is work now!</h2>")
 }
 
 func main() {
+	//
 	http.HandleFunc("/", handlerFunc)
+	// start server and listen on port 3000
+	//port is last part of URL, default for HTTP is 80
+	//when i type http://www.google.com the browser will use a default port:
+	//http://www.google.com:80
+	//nil means use a default http handlers
 	http.ListenAndServe(":3000", nil)
 }
