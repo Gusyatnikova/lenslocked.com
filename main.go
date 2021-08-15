4e11eb682b08cd4bdb629149ddc5edf068028fba
Gusyatnikova <crysnat08@gmail.com>
2021-08-15 18:46:57 +0500

    Added a content-type header

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 
 //handlerFunc process incoming web-requests
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Welcome to Natasha's awesome site!</h1>" +
-		"<h2> Fresh is work now!</h2>")
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, "<h1> Welcome to Natasha's awesome site!</h1>")
 }
 
 func main() {
-	//
 	http.HandleFunc("/", handlerFunc)
 	// start server and listen on port 3000
 	//port is last part of URL, default for HTTP is 80
