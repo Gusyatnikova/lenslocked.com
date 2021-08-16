64588c422200aa6e0833348b8362bb57f082df27
Gusyatnikova <crysnat08@gmail.com>
2021-08-16 09:29:35 +0500

    added a contact page

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 
 //handlerFunc process incoming web-requests
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, "<h1> Welcome to Natasha's awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1> Welcome to Natasha's awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a "+
+			"href=\"mailto:support@lenlocked.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {
