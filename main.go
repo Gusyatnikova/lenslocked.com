31a08784e506a5f7926e809459da916413d81826
Gusyatnikova <crysnat08@gmail.com>
2021-08-16 09:44:58 +0500

    added a 404 page

--- main.go
+++ main.go
@@ -13,15 +13,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a "+
 			"href=\"mailto:support@lenlocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h2>We could not find the page you were looking for :(</h2>" +
+			"<p>Plese email us if you keep being sent to an invalid page.</p>")
 	}
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	// start server and listen on port 3000
-	//port is last part of URL, default for HTTP is 80
-	//when i type http://www.google.com the browser will use a default port:
-	//http://www.google.com:80
-	//nil means use a default http handlers
 	http.ListenAndServe(":3000", nil)
 }
