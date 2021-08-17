a7c15bef0d2b210d90fc813b0f09b2d589dcc675
Gusyatnikova Natasha <gusiatnikovanatalia@gmail.com>
2021-08-17 13:10:54 +0500

    Added a FAQ page

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +17,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"href=\"mailto:support@lenlocked.com\">support@lenslocked.com</a>.")
 }
 
+func faq (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset-utf-8")
+	fmt.Fprint(w, "<h2>What is my mood today?</h2>" +
+		"<p>Looking forward to a productive day!</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
